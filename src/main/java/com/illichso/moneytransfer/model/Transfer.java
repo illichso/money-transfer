2306dd5c1f9f55178b79b92fdfb58a8f359b052a
illichso <illichso@gmail.com>
2017-12-29 13:28:19 +0100

    update

--- src/main/java/com/illichso/moneytransfer/model/Transfer.java
+++ src/main/java/com/illichso/moneytransfer/model/Transfer.java
@@ -1,4 +1,19 @@
 package com.illichso.moneytransfer.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Transfer {
+    @Id
+    private String id;
+    private int amount;
+    private String client;
+    private String description;
 }
