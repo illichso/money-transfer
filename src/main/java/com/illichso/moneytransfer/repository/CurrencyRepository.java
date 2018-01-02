6335ffef7157efbf597a7c56b1e6105eccfb4552
illichso <illichso@gmail.com>
2018-01-02 14:21:31 +0100

    update

--- src/main/java/com/illichso/moneytransfer/repository/CurrencyRepository.java
+++ src/main/java/com/illichso/moneytransfer/repository/CurrencyRepository.java
@@ -2,6 +2,8 @@ package com.illichso.moneytransfer.repository;
 
 import com.illichso.moneytransfer.model.Currency;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurrencyRepository extends ReactiveMongoRepository <Currency, String> {
 }
